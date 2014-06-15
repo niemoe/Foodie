6e6f3808dff18b21153733c0a98c59dded58aac0
Sebastian <sebastian.moeser@gmail.com>
2014-06-15 10:55:49 +0200

    Created FooSplitViewController and FooRootNavController

---
+++ Foodie/FOORootNavController.h
@@ -0,0 +1,13 @@
+//
+//  FOORootNavController.h
+//  Foodie
+//
+//  Created by Sebastian Möser on 15/06/14.
+//  Copyright (c) 2014 Modulum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FOORootNavController : UINavigationController
+
+@end
---
+++ Foodie/FOOSplitViewController.h
@@ -0,0 +1,13 @@
+//
+//  FOOViewController.h
+//  Foodie
+//
+//  Created by Sebastian Möser on 15/06/14.
+//  Copyright (c) 2014 Modulum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FOOSplitViewController : UIViewController
+
+@end
